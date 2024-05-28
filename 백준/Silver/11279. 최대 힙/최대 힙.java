741ddbbbb79e2234acb5a0a38c1e64418db2ce81
sangyong <82918538+gkdis6@users.noreply.github.com>
2024-05-28 22:39:18 +0900

    [Silver II] Title: 최대 힙, Time: 432 ms, Memory: 32236 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException{
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> que = new PriorityQueue<>((o1,o2) ->{
+            return o2-o1;
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<n; i++) {
+            int cur = Integer.parseInt(br.readLine());
+            if(cur == 0) {
+                if(que.isEmpty()) sb.append("0\n");
+                else sb.append(que.poll()+"\n");
+            }else {
+                que.add(cur);
+            }
+        }
+        System.out.println(sb);
+    }
+}
