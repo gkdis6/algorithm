f9bffee184e528324867ac0fbc2e85488138897f
sangyong <82918538+gkdis6@users.noreply.github.com>
2024-05-28 22:37:45 +0900

    [Silver II] Title: 최소 힙, Time: 296 ms, Memory: 27692 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -9,14 +9,16 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         PriorityQueue<Integer> que = new PriorityQueue<>();
 
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i<n; i++) {
             int cur = Integer.parseInt(br.readLine());
             if(cur == 0) {
-                if(que.isEmpty()) System.out.println("0");
-                else System.out.println(que.poll());
+                if(que.isEmpty()) sb.append("0\n");
+                else sb.append(que.poll()+"\n");
             }else {
                 que.add(cur);
             }
         }
+        System.out.println(sb);
     }
 }
