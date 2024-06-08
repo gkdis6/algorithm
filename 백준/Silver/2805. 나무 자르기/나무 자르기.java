ca2a197ea8fade682db3a54472ce70094b218cfc
sangyong <82918538+gkdis6@users.noreply.github.com>
2024-06-08 23:10:59 +0900

    [Silver II] Title: 나무 자르기, Time: 476 ms, Memory: 167832 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static int MOD = 1000000007;
+    static int[] arr;
+    static int n, k;
+    static long max = 0;
+
+    public static void main(String[] args) throws IOException{
+        st = new StringTokenizer(br.readLine());
+        k = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+
+        arr = new int[k];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<k; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, arr[i]);
+        }
+
+        find(arr);
+    }
+
+    public static void find(int[] arr){
+        long start = 0;
+        long end = max+1;
+        while(start < end){
+            long findPiece = 0;
+            long mid = (start + end)/2;
+            for(int i = 0; i<k; i++){
+                findPiece += mid < arr[i] ? arr[i] - mid : 0;
+            }
+            if(n > findPiece){
+                end = mid;
+            }else{
+                start = mid +1;
+            }
+        }
+        System.out.println(start -1);
+    }
+}
