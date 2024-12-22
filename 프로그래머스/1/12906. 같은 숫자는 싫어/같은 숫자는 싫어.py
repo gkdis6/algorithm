b0435a13234e8c2c5fb768e0f273809a63b9822c
sangyong <82918538+gkdis6@users.noreply.github.com>
2024-12-22 20:26:15 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 134.81 ms, Memory: 27.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,8 @@
+def solution(arr):
+    result = []
+    for i in range(len(arr)-1):
+        if arr[i] != arr[i+1]:
+            result.append(arr[i])
+        i += 1
+    result.append(arr[-1])
+    return result
