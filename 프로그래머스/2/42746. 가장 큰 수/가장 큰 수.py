84f460c5729e99dabe0749f9fa35d3b2ee481573
sangyong <82918538+gkdis6@users.noreply.github.com>
2024-12-25 15:36:34 +0900

    [level 2] Title: 가장 큰 수, Time: 338.91 ms, Memory: 24.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -2,6 +2,6 @@ import functools
 
 def solution(numbers):
     answer = ''.join(sorted([str(i) for i in numbers],
-                key=functools.cmp_to_key(lambda x, y: -1 if x + y > y + x else (1 if x + y < y + x else 0))))
+                key=functools.cmp_to_key(lambda x, y: -1 if x + y > y + x else 1)))
     
     return '0' if answer[0] == '0' else answer
