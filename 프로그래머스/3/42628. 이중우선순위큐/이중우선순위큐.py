81b2991a292eee03284f656fa3d1fd25511e7891
sangyong <82918538+gkdis6@users.noreply.github.com>
2024-12-25 15:02:41 +0900

    [level 3] Title: 이중우선순위큐, Time: 297.31 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,16 @@
+import heapq
+
+def solution(operations):
+    q = []
+    for operation in operations:
+        if operation[0] == 'I':
+            heapq.heappush(q, int(operation[2:]))
+        elif operation[0] == 'D':
+            if len(q) == 0: continue
+            elif operation[2:] == '-1':
+                print(heapq.heappop(q))
+            else:
+                largest = heapq.nlargest(1, q)[0]
+                q.remove(largest)
+    if len(q) < 1 : return [0,0]
+    return[heapq.nlargest(1, q)[0],heapq.heappop(q)]
