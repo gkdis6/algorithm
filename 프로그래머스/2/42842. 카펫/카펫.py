f30a8a1ffe59e8e7423fdc09445c7a653e8f4a06
SangYong <gkdis6@gmail.com>
2024-12-31 13:06:27 +0900

    edit comment

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,5 +1,5 @@
 def solution(brown, yellow):
     sumXY = (brown-4)/2 # 가로 + 세로
-    subXY = (sumXY * sumXY  - 4*yellow) ** 0.5 # 가로^2 + 세로^2 + 2*가로*세로
+    subXY = (sumXY * sumXY  - 4*yellow) ** 0.5 # 가로^2 + 세로^2 - 2*가로*세로
     print(sumXY, subXY)
     return [max(abs((sumXY+subXY)/2), abs((sumXY-subXY)/2))+2, min(abs((sumXY+subXY)/2), abs((sumXY-subXY)/2))+2]
