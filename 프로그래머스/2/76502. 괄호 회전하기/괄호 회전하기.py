1f5740a618b93e2cade2835e38ff45fcf9f32aea
sangyong <82918538+gkdis6@users.noreply.github.com>
2025-01-23 00:36:45 +0900

    [level 2] Title: 괄호 회전하기, Time: 90.69 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+def solution(s):
+    answer = 0
+    
+    def isValid(arr):
+        stack = []
+        for i in arr:
+            if i == '(' or i == '[' or i == '{':
+                stack.append(i)
+                continue
+            elif not stack:
+                return False
+                
+            before = stack.pop()
+            if i == ')' and before == '(':
+                continue
+            elif i == ']' and before == '[':
+                continue
+            elif i == '}' and before == '{':
+                continue
+            else:
+                return False
+        return len(stack) == 0
+    
+    arr = deque()
+    for i in s:
+        arr.append(i)
+    for _ in range(len(s)):
+        arr.rotate(1)
+        if isValid(arr):
+            answer += 1
+    
+    return answer
