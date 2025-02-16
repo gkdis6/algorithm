44f2bc55134e2d58cf95068ec9d69b1bb04cc022
sangyong <82918538+gkdis6@users.noreply.github.com>
2025-02-16 22:21:00 +0900

    [level 2] Title: 튜플, Time: 366.22 ms, Memory: 12.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = []
+    s = s[2:-2].split('},{')
+    s = sorted(s, key=len)
+    
+    for i in s:
+        numbers = i.split(',')
+        for number in numbers:
+            if int(number) not in answer:
+                answer.append(int(number))
+                
+    return answer
