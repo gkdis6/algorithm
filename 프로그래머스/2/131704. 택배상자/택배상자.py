ab1813c0fbe997e04f268f9d98da1a236d383b73
sangyong <82918538+gkdis6@users.noreply.github.com>
2025-03-03 11:07:12 +0900

    [level 2] Title: 택배상자, Time: 245.23 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -1,28 +1,11 @@
 def solution(order):
     answer = 0
-    cur=1
     stack = []
-    n = len(order)
     
-    for i in order:
-        if i == cur:
-            answer += 1
-            cur +=1
-            while stack and stack[-1] == cur:
-                stack.pop()
-                answer += 1
-                cur += 1
-        elif stack and i == stack[-1]:
+    for index, box in enumerate(order, start=1):
+        stack.append(index)
+        while stack and stack[-1] == order[answer]:
             stack.pop()
-            answer +=1
-        else:
-            while cur <= n and cur != i:
-                stack.append(cur)
-                cur += 1
-            if cur <= n and cur == i:
-                answer += 1
-                cur += 1
-            else:
-                break
+            answer += 1
     
     return answer
