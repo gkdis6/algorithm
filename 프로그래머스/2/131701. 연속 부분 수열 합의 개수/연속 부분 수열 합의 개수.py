67541e0f37e22523f290ff2361b60232cb2a27f4
sangyong <82918538+gkdis6@users.noreply.github.com>
2025-03-05 20:33:57 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 279.50 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,12 +1,11 @@
 def solution(elements):
     answer = set()
     n = len(elements)
-    extended = elements * 2
 
     for i in range(n):
         total = 0
         for j in range(i, i + n):
-            total += extended[j]
+            total += elements[j%n]
             answer.add(total)
     
     return len(answer)
